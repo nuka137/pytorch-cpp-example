0fe2c770f6ea6024b064bcdee51e965ad52cfd3b
nuka137 <n.a.sch137@gmail.com>
2020-07-29 20:58:21 +0900

    WIP

---
+++ yolo/README.md
@@ -0,0 +1,19 @@
+# Yolo
+
+Yolo model with C++/Python language.
+
+
+## Python
+
+### 1. Install PyTorch
+
+
+### 2. Download VOC
+
+Download MNIST dataset from [website](http://host.robots.ox.ac.uk/pascal/VOC/) and locate to `voc` directory.
+
+
+
+## References
+
+* [You Only Look Once: Unified, Real-Time Object Detection](https://arxiv.org/abs/1506.02640)
--- yolo/yolo.py
+++ yolo/yolo.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn.functional as F
 from torchsummary import summary
 
+NUMBER_OF_EPOCHS = 10
+LOG_INTERVAL = 100
+
+class YoloLoss(torch.nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, predict, target):
+        
+
 
 class YoloV1(torch.nn.Module):
     def __init__(self):
@@ -87,6 +97,19 @@ def set_random_seed(seed):
     torch.manual_seed(seed)
 
 
+def get_learning_rate(epoch, iteration, iteration_max):
+    if epoch == 0:
+        lr = 0.001 + (0.01 - 0.001) * iteration / iteration_max
+    elif 1 <= epoch < 76:
+        lr = 0.01
+    elif 76 <= epoch < 96:
+        lr = 0.001
+    else:
+        lr = 0.0001
+    
+    return lr
+
+
 def main():
     set_random_seed(1)
 
@@ -103,7 +126,50 @@ def main():
     model = YoloV1()
     model.to(device)
     summary(model, (3, 448, 448))
+    optimizer = torch.optim.SGD(model.parameters(), lr=get_learning_rate(0, 0, 0),
+                                momentum=0.9, weight_decay=0.0005)
 
+    # Load dataset.
+    iteration_max = len(train_loader)
+
+    
+    # Train loop.
+    for epoch in range(NUMBER_OF_EPOCHS):
+        print("Epoch {}: ".format(epoch))
+
+        # Train.
+        print("Start train.")
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            # Update learning rate.
+            for group in optimizer.param_groups:
+                group["lr"] = get_learning_rate(epoch, batch_idx, iteration_max)
+
+            optimizer.zero_grad()
+            data = data.to(device)
+            target = target.to(device)
+
+            output = model(data)
+
+            loss = ...
+            loss.backward()
+            optimizer.step()
+
+            if batch_idx % LOG_INTERVAL == 0:
+                print("Batch: {}, Loss: {}".format(batch_idx, loss.item()))
+        
+        # Evaluate.
+        # print("Start eval.")
+        # model.eval()
+        # with torch.no_grad():
+        #     for data, target in test_loader:
+        #         data = data.to(device)
+        #         target = target.to(device)
+
+        #         output = model(data)
+
+        #         loss = ...
+                
 
 
 if __name__ == "__main__":
